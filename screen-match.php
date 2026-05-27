9dc7030400afcb89cdd58e6fee1728fb13f6f835
kauezxl <leitekaue@gmail.com>
2026-05-26 21:24:28 -0300

    removendo teste de casa

--- screen-match.php
+++ screen-match.php
@@ -12,8 +12,6 @@ $sumOfNotes = 0;
 
 $numberOfRatings = $argc -1;
 
-$teste = "teste";
-
 //for (inicializacao; condicao da repeticao, incremento)
 for ($contador = 1; $contador < $argc; $contador++) {
     $sumOfNotes += $argv[$contador];
