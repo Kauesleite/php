5110a94774566257f6e4a6989491d7c5a3d60af9
Kaue Leite <leitekaue@gmail.com>
2026-07-01 17:31:50 -0300

    enum genero

---
+++ screen-match/src/Modelo/Genero.php
@@ -0,0 +1,10 @@
+<?php
+
+enum Genero: string {
+    case ACAO = 'Ação';
+    case COMEDIA = 'Comédia';
+    case DRAMA = 'Drama';
+    case FICCAO_CIENTIFICA = 'Ficção Científica';
+    case ROMANCE = 'Romance';
+
+}
